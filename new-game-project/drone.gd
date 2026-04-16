181ec1b9b29acc920afb70afa0c3153804a7fe6f
Keshav Sumitran <61526291+AgeOfCheese@users.noreply.github.com>
2026-04-15 20:34:15 -0700

    finished basic fps controller setup future ui

--- new-game-project/drone.gd
+++ new-game-project/drone.gd
@@ -3,6 +3,16 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.03
+
+@onready var head = $Head
+@onready var camera_3d = $Head/Camera3D
+
+
+
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 func _physics_process(delta):
@@ -16,8 +26,8 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("left", "right", "forward", "back")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -26,3 +36,8 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera_3d.rotate_x(-event.relative.y * SENSITIVITY)
+		camera_3d.rotation.x = clamp(camera_3d.rotation.x,deg_to_rad(-60), deg_to_rad(60))
